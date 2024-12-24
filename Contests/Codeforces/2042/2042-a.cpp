3fbcab58fb31bed018afaa19c506cb6ba8418f9b
Ratchanon <ratchanonmcnon2015@gmail.com>
2024-12-24 21:18:34 +0700

    Update

---
+++ Contests/Codeforces/2042/2042-a.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve()
+{
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; ++t)
+    {
+        solve();
+        cout << endl;
+    }
+    return 0;
+}
